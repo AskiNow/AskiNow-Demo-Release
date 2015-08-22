5e348ab19d4f8e829f255b4392fdaa380f86d0b5
Vincent Zhao <vincentzhaorz@gmail.com>
2015-08-22 15:49:10 +0000

    revised apidoc.json

--- app/apidoc/api_project.js
+++ app/apidoc/api_project.js
@@ -1,13 +1,13 @@
 define({
-  "name": "example",
+  "name": "AskiNow",
   "version": "0.1.0",
   "description": "AskiNow API Document",
-  "url": "https://api.askinow.com/v1",
+  "url": "https://askinow.com/api/",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-08-22T15:18:42.381Z",
+    "time": "2015-08-22T15:48:17.802Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
